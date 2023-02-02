153d58fb62da9d954c7ecc6d193bef3125ec02a3
DhanaSrinivas <dhanasrinivas20@gmail.com>
2023-02-03 00:46:50 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+def print_picture(number):
+    if number == 0:
+        print(rock)
+    elif number == 1:
+        print(paper)
+    elif number == 2:
+        print(scissors)
+    else:
+        print("Invalid Choice of Number")
+
+
+print("Welcome to Rock Paper Scissors game")
+
+your_choice = int(input("What do you choose ? Type 0 for Rock🪨, 1 for paper📄, 2 for scissors✂️\n"))
+print_picture(your_choice)
+
+print("Computer chose: ")
+computer_choice = random.randint(0, 2)
+print_picture(computer_choice)
+
+if your_choice == computer_choice:
+    print("Game drawn😛")
+elif your_choice == 0 and computer_choice == 2:
+    print("You won🥳")
+elif your_choice == 2 and computer_choice == 0:
+    print("You lose😢")
+elif your_choice < computer_choice:
+    print("You lose😢")
+else:
+    print("You won🥳")
